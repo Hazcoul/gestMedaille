92e387634121fe3b667f0132eaabe94b7ffbf775
Osie68 <sielamoussaouattara@gmail.com>
2023-12-17 23:07:54 +0000

    Initial commit with gitignore config done

---
+++ stock-de-medaille/src/main/java/bf/gov/gcob/medaille/StockDeMedailleApplication.java
@@ -0,0 +1,13 @@
+package bf.gov.gcob.medaille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockDeMedailleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockDeMedailleApplication.class, args);
+	}
+
+}
---
+++ stock-de-medaille/src/test/java/bf/gov/gcob/medaille/StockDeMedailleApplicationTests.java
@@ -0,0 +1,13 @@
+package bf.gov.gcob.medaille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDeMedailleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
