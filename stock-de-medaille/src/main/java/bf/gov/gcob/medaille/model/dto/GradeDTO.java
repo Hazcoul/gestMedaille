7c2fe8df7b2442ccbbd0b3964fc9e21185c86851
HIEN Pierre Canisius <canisiushien@gmail.com>
2024-01-04 15:30:24 +0000

    add endpoint of updateDtoWithImageMedaille

--- stock-de-medaille/src/main/java/bf/gov/gcob/medaille/model/dto/GradeDTO.java
+++ stock-de-medaille/src/main/java/bf/gov/gcob/medaille/model/dto/GradeDTO.java
@@ -5,14 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class GradeDTO extends AbstractBaseDTO {
 
-	private Long        idGrade;
-    private String      typeGrade;
-    private String      libelle;
-    private String      description;
+    private Long idGrade;
+    private String typeGrade;
+    private int code;//1 pour Chevalier, 2:Officier, 3:Commandeur, 4:Grand-officier et 5:Grand-croix
+    private String libelle;
+    private String description;
 }
