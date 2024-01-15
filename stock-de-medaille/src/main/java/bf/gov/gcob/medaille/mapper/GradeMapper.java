ff9c9b990b3b19e11666e4eae54fe46376b9c1cb
HIEN Pierre Canisius <canisiushien@gmail.com>
2024-01-15 10:10:28 +0000

    update gradeMapper adding setCode

--- stock-de-medaille/src/main/java/bf/gov/gcob/medaille/mapper/GradeMapper.java
+++ stock-de-medaille/src/main/java/bf/gov/gcob/medaille/mapper/GradeMapper.java
@@ -8,31 +8,33 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GradeMapper extends AbstractBaseMapper {
-
+    
     public GradeDTO toDTO(Grade entity) {
         if (Objects.isNull(entity)) {
             return null;
         }
-
+        
         GradeDTO dto = new GradeDTO();
         dto.setDescription(entity.getDescription());
         dto.setIdGrade(entity.getIdGrade());
+        dto.setCode(entity.getCode());
         dto.setLibelle(entity.getLibelle());
         dto.setTypeGrade(entity.getTypeGrade().getLibelle());
         setCommonFieldsFromEntity(entity, dto);
-
+        
         return dto;
     }
-
+    
     public Grade toEntity(GradeDTO dto) {
         Grade entity = new Grade();
         entity.setDescription(dto.getDescription());
         entity.setIdGrade(dto.getIdGrade());
+        entity.setCode(dto.getCode());
         entity.setLibelle(dto.getLibelle());
         entity.setTypeGrade(ETypeGrade.getByLibelle(dto.getTypeGrade()));
         setCommonFieldsFromDTO(dto, entity);
-
+        
         return entity;
     }
-
+    
 }
